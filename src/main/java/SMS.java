c3c960da49b26aee1860881597cecf4b127764e6
pdalziel <mail@pdalziel.xyz>
2017-01-22 17:16:21 +0000

    added SMS class

---
+++ src/main/java/SMS.java
@@ -0,0 +1,39 @@
+public class SMS {
+
+    String card="", PIN="", option="";
+    String fromnumber="";
+    boolean validSMS=false;
+    String originalsms="";
+
+
+    public SMS() {
+    }
+
+    public SMS(String _from, String _card, String _PIN, String _option, String _orig) {
+        card = _card;
+        fromnumber=_from;
+        PIN = _PIN;
+        option = _option;
+        originalsms=_orig;
+        validSMS=true;
+    }
+
+    public boolean isNumber(String n) {
+        try {
+            Integer i = new Integer(n);
+            return true;
+        }
+        catch (Exception ee) {
+            return false;
+        }
+    }
+
+
+
+    public void print() {
+        if (validSMS)
+            System.out.println("card="+card+",pin="+PIN + " option= " + option);
+        else System.out.println("Invalid message");
+
+    }
+}
