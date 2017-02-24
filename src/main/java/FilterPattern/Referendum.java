3ef9ba6da6108ddf5558886f8a059b1d6ef26f97
pdalziel <mail@pdalziel.xyz>
2017-02-24 08:00:43 +0000

    Merge branch 'master' of /home/p/Projects/handivote with conflicts.

--- src/main/java/FilterPattern/Referendum.java
+++ src/main/java/FilterPattern/Referendum.java
@@ -1,21 +1,5 @@
 package FilterPattern;
 
 
-import java.util.Date;
-import java.util.UUID;
-
-public class Referendum implements ReferendumFactory{
-
-    public Referendum() {
-    }
-
-    @Override
-    public UUID createReferendum(Date startDate, Date endDate, Question question, VoteCollector collector) {
-        return null;
-    }
-
-    @Override
-    public void publishResults(UUID refId) {
-
-    }
+public class Referendum {
 }
