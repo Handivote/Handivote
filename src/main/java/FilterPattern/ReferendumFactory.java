10b29aa80c8d8f09bed9040e5961c523b54e9195
pdalziel <mail@pdalziel.xyz>
2017-02-26 15:03:10 +0000

    more work on  ref.manager and properties

--- src/main/java/FilterPattern/ReferendumFactory.java
+++ src/main/java/FilterPattern/ReferendumFactory.java
@@ -1,14 +1,68 @@
 package FilterPattern;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Properties;
+
 public class ReferendumFactory {
 
-    public Referendum createReferendum(String type){
-        switch (type){
+     void readProperties(Properties properties) {
+         String pattern = properties.getProperty("dates.pattern");
+         String startDateStr = properties.getProperty("startDate");
+         String endDateStr = properties.getProperty("endDate");
+         String refType = properties.getProperty("refType");
+         Question[] questions;
+         for ( int i =0; i < new Integer(properties.getProperty("numberOfQuestions"));i++){
+             ArrayList<QuestionOption> options = new ArrayList<>();
+             for(int j = 1; j<= new Integer(properties.getProperty("numberOfOptions")); j++){
+                 options.add(new QuestionOption(""+ j ,"" + j,  properties.getProperty("questionOption"+ j)));
+             }
+             Question question = new Question(1, 0, properties.getProperty("question1"), options );
+             //questions[i]=
+             //System.out.println(question.toString());
+         }
+         Date startDate = setDates(pattern, startDateStr);
+         Date endDate = setDates(pattern, endDateStr);
+     }
+
+    private static Date setDates(String pattern, String dateStr) {
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        Date date = null;
+        try {
+            date = format.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public Referendum createReferendum(Properties properties){
+        String pattern = properties.getProperty("dates.pattern");
+        String startDateStr = properties.getProperty("startDate");
+        String endDateStr = properties.getProperty("endDate");
+        Date startDate = setDates(pattern, startDateStr);
+        Date endDate = setDates(pattern, endDateStr);
+
+        String refType = properties.getProperty("refType");
+        Question[] questions;
+        for ( int i =0; i < new Integer(properties.getProperty("numberOfQuestions"));i++){
+            ArrayList<QuestionOption> options = new ArrayList<>();
+            for(int j = 1; j<= new Integer(properties.getProperty("numberOfOptions")); j++){
+                options.add(new QuestionOption(""+ j ,"" + j,  properties.getProperty("questionOption"+ j)));
+            }
+            Question question = new Question(1, 0, properties.getProperty("question1"), options );
+
+            System.out.println(question.toString());
+        }
+
+        switch (refType){
             case "simple":
                 SimpleReferendum simpleReferendum = new SimpleReferendum();
                 break;
             default:
-                throw new IllegalArgumentException("Invalid Referendum type: " + type);
+                throw new IllegalArgumentException("Invalid Referendum type: " + refType);
 
         }
 
--- src/main/java/FilterPattern/ReferendumManager.java
+++ src/main/java/FilterPattern/ReferendumManager.java
@@ -2,18 +2,16 @@ package FilterPattern;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Enumeration;
 import java.util.Properties;
 
 public class ReferendumManager {
     private static ArrayList <String> props;
 
     public static void main(String[] args) {
+        ReferendumFactory rf = new ReferendumFactory();
         String fName = "";
         if(args.length >0){
             fName = args[0];
@@ -22,57 +20,31 @@ public class ReferendumManager {
             fName = "test.properties";
         }
 
-        readProperties(fName);
-        ReferendumFactory rf = new ReferendumFactory();
-        Referendum referendum = rf.createReferendum("simple");
-
-
-    }
-
-    private static void readProperties(String fName) {
+        File file = new File(fName);
+        FileInputStream fileInput = null;
         try {
-            File file = new File(fName);
-            FileInputStream fileInput = new FileInputStream(file);
-            Properties properties = new Properties();
+            fileInput = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Properties properties = new Properties();
+        try {
             properties.load(fileInput);
-            String pattern = properties.getProperty("dates.pattern");
-            String startDateStr = properties.getProperty("startDate");
-            String endDateStr = properties.getProperty("endDate");
-            String refType = properties.getProperty("refType");
-            Question[] questions;
-            for ( int i =0; i < new Integer(properties.getProperty("numberOfQuestions"));i++){
-                ArrayList <QuestionOption> options = new ArrayList<>();
-                for(int j = 1; j<= new Integer(properties.getProperty("numberOfOptions")); j++){
-                    options.add(new QuestionOption(""+ j ,"" + j,  properties.getProperty("questionOption"+ j)));
-                }
-                Question question = new Question(1, 0, properties.getProperty("question1"), options );
-                //questions[i]=
-                System.out.println(question.toString());
-            }
-            Date startDate = setDates(pattern, startDateStr);
-            Date endDate = setDates(pattern, endDateStr);
-            fileInput.close();
-
-            Enumeration enuKeys = properties.keys();
-            while (enuKeys.hasMoreElements()) {
-                String key = (String) enuKeys.nextElement();
-                String value = properties.getProperty(key);
-                //System.out.println(key + ": " + value);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static Date setDates(String pattern, String dateStr) {
-        SimpleDateFormat format = new SimpleDateFormat(pattern);
-        Date date = null;
+        System.out.println(properties.stringPropertyNames());
+        rf.createReferendum(properties);
         try {
-            date = format.parse(dateStr);
-        } catch (ParseException e) {
+            fileInput.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return date;
+
+        //fName);
+
+
+
     }
 
 }
